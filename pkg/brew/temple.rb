b8ee33b8e64072c10a526c6e97be290eb2cd6b86
Ben Wilber <benwilber@gmail.com>
2021-10-10 17:21:56 -0400

    Updating Homebrew packages

--- pkg/brew/temple.rb
+++ pkg/brew/temple.rb
@@ -1,14 +1,14 @@
 class Temple < Formula
-  version "0.3.0"
+  version "0.3.1"
   desc "A commandline program that renders template files with structured context inputs. It is most often used to transform JSON data from a web API to a presentation format such as HTML."
   homepage "https://github.com/benwilber/temple"
 
   if OS.mac?
     url "https://github.com/benwilber/temple/releases/download/#{version}/temple-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "425e97bd31105f36d69095438055e2e2ca42dd7af32fb5d6dc10d4fe17dc3e72"
+    sha256 "3bd628d91b74cc53ab06119c818da1ddbcb342190feb2b75c783144b5235d5d4"
   elsif OS.linux?
     url "https://github.com/benwilber/temple/releases/download/#{version}/temple-#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "c43e407337105bf7716d2162efbc377dab8f7737475f5d08c0ed8ede0a08d4fb"
+    sha256 "da519505de98116ac417f70a425bc9c4c8a909919fb6abfcce2b0e197d296abf"
   end
 
   def install
