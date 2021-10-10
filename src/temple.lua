ad9e8e99dbd6d16cb42363d2af1da2b93a508fc9
Ben Wilber <benwilber@gmail.com>
2021-10-10 19:23:40 -0400

    Removing unused code, for now.

--- src/temple.lua
+++ src/temple.lua
@@ -1,18 +1,8 @@
 temple = {
   _filters = {},
-  _functions = {},
-  _tests = {},
 
   addfilter = function(self, name, func)
     self._filters[name] = func
-  end,
-
-  addfunction = function(self, name, func)
-    self._functions[name] = func
-  end,
-
-  addtest = function(self, name, func)
-    self._tests[name] = func
   end
 }
 
